51c53263871390f72cb7adc61a54dc8dedd5f847
Ali Toosi <alirezakhoshghalb@ymail.com>
2022-07-09 19:15:08 +1000

    Add all files

--- src/opentgbots/settings.py
+++ src/opentgbots/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") in (1, '1', 'True', 'TRUE')
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
